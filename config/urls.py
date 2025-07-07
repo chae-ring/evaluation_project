64f550b0eabc11bad77a7d46a3c8a39cdbed654a
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-07-07 10:40:55 +0900

    feat:login,logout,signup

--- config/urls.py
+++ config/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('project/<int:id>/result/', views.project_result, name='project_result'),
 
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='project_list'), name='logout'),
-
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', views.signup, name='signup'),
 
     path('admin/', admin.site.urls),
